fa48c04dc9547fc4a1372a56147d3e8301f15187
Francisco Javier Mendoza Vazquez <francisco.mendoza@softtekprojects.com>
2021-10-03 00:38:16 -0500

    java class upadate

--- src/generalPakage/tests.java
+++ src/generalPakage/tests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class LogIn {
+public class tests {
   @Test
   public void f() {
   }
