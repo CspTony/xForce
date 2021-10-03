de85537d9268a20b7516b71aa109c7dfe3c1e4f2
Francisco Javier Mendoza Vazquez <francisco.mendoza@softtekprojects.com>
2021-10-03 00:03:30 -0500

    first commit

---
+++ src/generalPakage/LogIn.java
@@ -0,0 +1,19 @@
+package generalPakage;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LogIn {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
