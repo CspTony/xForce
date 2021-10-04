330200f3717c9266d765ade77277c10709bfd1ca
Francisco Javier Mendoza Vazquez <francisco.mendoza@softtekprojects.com>
2021-10-04 15:10:41 -0500

    Log in comented

--- src/demoBlaze/LogIn.java
+++ src/demoBlaze/LogIn.java
@@ -8,18 +8,30 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LogIn extends PageTemplate {
 	
+	/*
+	 * Xpath Values for Website elements
+	 */
 	private String logInXpath = "//*[@id=\"login2\"]";
 	private String logPopTitXpath = "//*[@id=\"logInModalLabel\"]";
 	private String userXpath = "//*[@id=\"loginusername\"]";
 	private String passwordXpath = "//*[@id=\"loginpassword\"]";
 	private String logInBXpath = "//*[@id=\"logInModal\"]/div/div/div[3]/button[2]";
 	private String LoggedInXpath = "//*[@id=\"nameofuser\"]";
-	//*[@id="nameofuser"]
+	
+	/*
+	 * Constructor for WebDriver
+	 * Redirecting to homepage.
+	 */
 	public LogIn(WebDriver driver) {
 		super(driver);
 		this.driver.get("https://www.demoblaze.com/");//Url
 	}
 	
+	/*
+	 * Function for loggin in the website.
+	 * @username (string) = the name of the username to be send to log in.
+	 * @password (String) = the value of the password to be send to log in.
+	 */
 	public void logginIn(String username, String password){
 		WebElement logButton = this.driver.findElement(By.xpath(logInXpath));
 		logButton.click();
@@ -34,6 +46,7 @@ public class LogIn extends PageTemplate {
 		signUpButton.click();
 	}
 	
+	//Function that returns the string value of the Logged in text
 	public String loggedIn() throws InterruptedException {
 		Thread.sleep(2000);
 		WebDriverWait wait = new WebDriverWait(this.driver, 5);
