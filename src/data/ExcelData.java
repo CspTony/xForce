c021f810c54139a9630fa6418ca252375001c099
Francisco Javier Mendoza Vazquez <francisco.mendoza@softtekprojects.com>
2021-10-04 16:07:18 -0500

    ExcelData commentes updated

--- src/data/ExcelData.java
+++ src/data/ExcelData.java
@@ -8,8 +8,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class ExcelData {
-	/*User Functions
-	 *  Obtains the test user data
+	
+	/*User Function
+	 *  Obtains the test username data
 	 *  @idUser (int) = user index in the excel sheet
 	 * */
 	public String username(int idUser) throws IOException {
@@ -19,6 +20,10 @@ public class ExcelData {
 		return username;
 	}
 	
+	/*User Function
+	 *  Obtains the test password data
+	 *  @idUser (int) = password index in the excel sheet
+	 * */
 	public String password(int idUser) throws IOException {
 		
 		String password = this.readData(idUser, 1, 0);
@@ -26,6 +31,10 @@ public class ExcelData {
 		return password;
 	}
 	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = complete name index in the excel sheet
+	 * */
 	public String userCompleteName(int idUser) throws IOException {
 		
 		String usertCompleteName = this.readData(idUser, 2, 0);
@@ -33,6 +42,10 @@ public class ExcelData {
 		return usertCompleteName;
 	}
 	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = country index in the excel sheet
+	 * */
 	public String userCountry(int idUser) throws IOException {
 		
 		String userCountry = this.readData(idUser, 3, 0);
@@ -40,18 +53,32 @@ public class ExcelData {
 		return userCountry;
 	}
 	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = city index in the excel sheet
+	 * */
 	public String userCity(int idUser) throws IOException {
 		
 		String userCity = this.readData(idUser, 4, 0);
 		
 		return userCity;
 	}
+	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = CardNumber index in the excel sheet
+	 * */
 	public String userCardNumber(int idUser) throws IOException {
 		
 		String userCardNumber = this.readData(idUser, 5, 0);
 		
 		return userCardNumber;
 	}
+	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = card Month index in the excel sheet
+	 * */
 	public String userCardMonth(int idUser) throws IOException {
 		
 		String userCardMonth = this.readData(idUser, 6, 0);
@@ -59,6 +86,10 @@ public class ExcelData {
 		return userCardMonth;
 	}
 	
+	/*User Functions
+	 *  Obtains the test user data
+	 *  @idUser (int) = Card year index in the excel sheet
+	 * */
 	public String userCardYear(int idUser) throws IOException {
 		
 		String userCardYear = this.readData(idUser, 7, 0);
@@ -78,6 +109,7 @@ public class ExcelData {
 	
 	/*
 	 * Obtains info stored un excel sheet
+	 * Returns the String value of the cell
 	 * */
 	
 	public String readData(int row, int column, int exsheet) throws IOException{
